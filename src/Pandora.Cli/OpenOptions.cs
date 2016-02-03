b58426516bad2d66947550034f2128b8687bb967
Sinstraliz <blagovest.vp@gmail.com>
2016-02-03 16:00:28 +0200

    Moved Pandora.Cli to separate repository and added the new Pandora.

---
+++ src/Pandora.Cli/OpenOptions.cs
@@ -0,0 +1,25 @@
+﻿using CommandLine;
+using CommandLine.Text;
+
+namespace Elders.Pandora.Cli
+{
+    public class OpenOptions
+    {
+        public const string EnvVarOutput = "envvar";
+
+        [Option('j', "jar", HelpText = "Input jar file")]
+        public string Jar { get; set; }
+
+        [Option('c', "cluster", HelpText = "Cluster name")]
+        public string Cluster { get; set; }
+
+        [Option('m', "machine", HelpText = "Machine name")]
+        public string Machine { get; set; }
+
+        [Option('a', "application", HelpText = "Application name")]
+        public string Application { get; set; }
+
+        [Option('o', "output", DefaultValue = EnvVarOutput, HelpText = "Output")]
+        public string Output { get; set; }
+    }
+}
